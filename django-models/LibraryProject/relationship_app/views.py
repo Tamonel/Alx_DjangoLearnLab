824b6891f3d0562cf628b51ff3eaea0caa790cfd
Tamonel <motachikelvin@gmail.com>
2025-11-18 16:35:25 +0300

    ALX: Fix exact imports for checker compliance

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book
 from .models import Library
+from .models import Book
 
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
 
