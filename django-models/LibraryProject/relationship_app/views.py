7b3520ef08f545043acfbbba87f9e83faef488a5
Tamonel <motachikelvin@gmail.com>
2025-11-18 15:10:09 +0300

    ALX Django: Fix class-based view, function-based view, and URLs

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
@@ -10,6 +10,6 @@ def list_books(request):
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
